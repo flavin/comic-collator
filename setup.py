d0bf32ec3cd7df8f0b0ab27ec5a162589e97c724
Francisco Lavin <fcolavin@gmail.com>
2020-09-22 08:44:17 -0300

    bump version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="comic-collator",
-    version="0.2",
+    version="0.3",
     description="collate image files and concat images ready to print.",
     license="MIT",
     long_description=long_description,
